e71bc33188b4ecd543ede660a3d9c1eece874497
claudiobailon <c.bailonschubert@gmail.com>
2020-06-23 15:14:23 -0700

    Added event listener

--- js/results.js
+++ js/results.js
@@ -10,3 +10,17 @@
 
 //we will need to access some of the things from app.js here 
 // we're using the links constructor here to show results and links
+
+var transferInfoToResultsPage = document.getElementById('user-input');//Maybe submit-user
+transferInfoToResultsPage.addEventListener('submit', handleDataFromForm);//Is submit a valid action to listen to?
+
+function handleDataFromForm (event){
+  event.preventDefault();
+  var playerName = event.target.name.value;
+  var playerAge = event.target.age.value;
+  var interests1 = event.target.interest1.checked;
+  var interests2 = event.target.interest2.checked;
+  var interests3 = event.target.interest3.checked;//nick helped us find this https://stackoverflow.com/questions/14544104/checkbox-check-event-listener
+  console.log('***', interests1, interests2, interests3);
+}
+
